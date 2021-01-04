14814ef270a2afb7bdd5632e2b6929cffac65f9e
shiva341 <shivatejaprof@gmail.com>
2021-01-04 23:31:27 +0530

    added madlibs project

---
+++ madlibs.py
@@ -0,0 +1,35 @@
+topic=input("choose any topic 1.Self introduction, 2.story :")
+if topic=='1':
+    name=input('Enter your name;')
+    location=input('Enter the place you live:')
+    education=input('Enter your highest education:')
+    college=input('Enter your college;')
+    year=input('Enter year of graduation:')
+    ts=input('Enter your Thechnical skills:')
+    language=input('Enter the languages you know:')
+    hobbies=input('Enter your hobbies:')
+    family=input('Enter the number of people in your family:')
+    dad=input('Enter your dad occupation:')
+    mom=input('Enter your mom occupation:')
+    print('Hi I am {}.I am from {}.I completed my {} from {} in {}.I have knowledge in {}.I am fluent in {}.coming to my hobbies i like {}.And about my familywe are {}.my dad is a {},my mom is a {}.That is all about my self. '.format(name,location,education,college,year,ts,language,hobbies,family,dad,mom))
+else:
+    adj1=input('Enter a adjective1:')
+    adj2=input('Enter a adjective2:')
+    bird=input('Enter a bird name:')
+    room=input('Enter the name of a room:')
+    verb1=input('Enter a verb(past tense):')
+    verb2=input('Enter a verb:')
+    relative=input('Enter your relative name:')
+    noun1=input('Enter a noun:')
+    liquid=input('Enter any liquid:')
+    verb3=input('Enter a verb(-ing):')
+    part=input('Enter a part of body:')
+    noun2=input('Enter a noun (plural):')
+    verb4=input('Enter a verb(-ing):')
+    noun3=input('Enter a noun:')
+    print("It was a {},cold november day.I woke up to the {} smell of {} roasting in the {} downstairs.I {} down the stairs to see if i could help {} the dinner.My mom said 'see if  {} needs a fresh {}.'so i carried a tray of glasses full of {} into the {} room.When i got there i could'nt belive my {}! There were {} {} on the {}!".format(adj1,adj2,bird,room,verb1,verb2,relative,noun1,liquid,verb3,part,noun2,verb4,nsoun3))
+
+
+
+
+
